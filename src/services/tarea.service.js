5696ea410dbc58dff3b6f803e1bc4f14f04580a2
NoeliaVasquezCrespo <noelia.vasquez.crespo@gmail.com>
2026-02-20 15:32:28 -0400

    Create task.service file

---
+++ src/services/tarea.service.js
@@ -0,0 +1,22 @@
+const API_URL = "http://localhost:8000/api/tasks";
+
+
+export const getAll = async () => {
+
+};
+
+export const show = async (id) => {
+  
+};
+
+export const create = async (task) => {
+  
+};
+
+export const update = async (id, task) => {
+  
+};
+
+export const remove = async (id) => {
+  
+};
